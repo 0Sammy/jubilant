f7c84df544c2ff336866a9d18195789333758b5b
Priest <Charleschukwuemeka47@gmail.com>
2024-02-25 23:59:05 +0100

    Feat: Add the error pages correct the tidio link

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,5 +1,8 @@
 'use client'
- 
+
+//Import Needed Icons
+import { EmojiSad } from "iconsax-react";
+
 export default function GlobalError({
   error,
   reset,
@@ -10,8 +13,14 @@ export default function GlobalError({
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <main className="h-full min-h-screen bg-[#f0f0f0] flex flex-col items-center justify-center text-center p-8">
+              <EmojiSad size="60" className="text-slate-800"/>
+              <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mt-8">Page is not found</h1>
+              <div className="mt-8 text-sm md:text-base xl:text-lg font-medium">
+                 <p>An error was encountered.</p>
+                  <p className="mt-2">Kindly go back</p> 
+              </div>
+        </main>
       </body>
     </html>
   )
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ import { EmojiSad } from "iconsax-react";
 
 const Error = () => {
     return ( 
-        <main className="h-full min-h-screen bg-[#f0f0f0] flex flex-col items-center justify-center text-center">
+        <main className="h-full min-h-screen bg-[#f0f0f0] flex flex-col items-center justify-center text-center p-8">
             <EmojiSad size="60" className="text-slate-800"/>
             <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-bold mt-8">Page is not found</h1>
             <div className="mt-8 text-sm md:text-base xl:text-lg font-medium">
